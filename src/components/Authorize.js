75dc3572d51e0427048ffc22d5ed117591fb7224
mruhlin-stytch <130408010+mruhlin-stytch@users.noreply.github.com>
2025-05-07 21:24:47 -0400

    Add authorize component! (#134)

---
+++ src/components/Authorize.js
@@ -0,0 +1,16 @@
+import { Redirect } from 'react';
+import { IdentityProvider, useStytchUser } from '@stytch/react';
+
+const Authorize = () => {
+  const { user } = useStytchUser();
+
+  if (!user) {
+    const loginURL = `/?returnTo=${window.location.toString()}`
+    return <Redirect to={loginURL} />
+  }
+
+  return <IdentityProvider />
+
+};
+
+export default Authorize; 
